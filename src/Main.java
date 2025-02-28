610a509c43f45aad6e02e325fd8e640d0802c805
kerbilliam <168518734+kerbilliam@users.noreply.github.com>
2025-02-28 15:18:56 -0800

    Added database documentation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Task-Master!");
-		// Database.init();
-		// Database.addTask("test", "William", "Urgent", "pending");
-		// Database.deleteTask("test");
-		Database.printTable("tasks");
 	}
 }
