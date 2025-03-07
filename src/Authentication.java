965b884e9d2b76402ce2b51e79ac0054ab02b15e
jackcurry <jackbcurry@icloud.com>
2025-03-06 17:11:17 -0800

    added the isregistereduser method

---
+++ src/Authentication.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import DB.*;
+
+
+public class Authentication {
+    public static void main(String[] args) {
+
+    }
+
+    private HashMap<String, String> userDatabase;
+
+    public Authentication() {
+        this.userDatabase = Database.getAccounts();
+    }
+
+    public boolean isRegisteredUser(String inputUsername, String inputPassword) {
+        return userDatabase.containsKey(inputUsername) && userDatabase.get(inputUsername).equals(inputPassword);
+    }
+
+
+
+
+
+
+
+}
