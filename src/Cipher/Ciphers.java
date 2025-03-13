995c6f8fbe561faaddcc9f211a99d244772fd2a9
daniilnahl <daniil.nahl@protonmail.com>
2025-03-13 11:56:08 -0700

    Fixed a bug where multile invalid key statements would print when calling commands.
    Implemented error handling.

--- src/Cipher/Ciphers.java
+++ src/Cipher/Ciphers.java
@@ -1,9 +1,9 @@
 package Cipher;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class Ciphers {
@@ -16,17 +16,27 @@ public class Ciphers {
     }
     
     public static String encrypt(String plainText, String key) throws Exception  {
-        Key secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM); 
-        Cipher cipher = Cipher.getInstance(TRANSFORMATION); 
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-        byte[] byteForm = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8)); 
-        return Base64.getEncoder().encodeToString(byteForm); 
+        try {
+            Key secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM); 
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION); 
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+    
+            byte[] byteForm = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8)); 
+            return Base64.getEncoder().encodeToString(byteForm);
+        } catch (Exception e) {
+            System.err.println("Error occurred during encryption: " + e);
+            return "";
+        }
+ 
     }
 
 
     public static String decrypt(String cipherText, String key) throws Exception {
         try{
+            if (cipherText == null || cipherText.length() == 0) {
+                return "";
+            }
+
             Key secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM);
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
@@ -34,9 +44,9 @@ public class Ciphers {
 
             return new String(decryptedBytes, StandardCharsets.UTF_8);
             
-        } catch (Exception e){
-            System.out.println("Invalid key");
-            return "none";
+        } catch (IllegalArgumentException | IllegalStateException | javax.crypto.BadPaddingException | javax.crypto.IllegalBlockSizeException e){
+            System.err.println("Error occurred during decryption: " + e);
+            return "";
         }
             
 
