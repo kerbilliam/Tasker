8efcd45467165a274b4310d7443ab44752a54482
kerbilliam <168518734+kerbilliam@users.noreply.github.com>
2025-03-13 18:39:17 -0700

    added methods to make colors easier

--- src/Colors/StrColor.java
+++ src/Colors/StrColor.java
@@ -16,4 +16,37 @@ public class StrColor {
     public static final String CYAN = "\033[36m"; // Cyan color
     public static final String WHITE = "\033[37m"; // White color
     public static final String BLACK = "\033[30m"; // Black color
+    
+    public static String red(String input) {
+        return RED+input+RESET;
+    }
+    
+    public static String green(String input) {
+        return GREEN+input+RESET;
+    }
+
+    public static String yellow(String input) {
+        return YELLOW+input+RESET;
+    }
+
+    public static String blue(String input) {
+        return BLUE+input+RESET;
+    }
+
+    public static String magenta(String input) {
+        return MAGENTA+input+RESET;
+    }
+
+    public static String cyan(String input) {
+        return CYAN+input+RESET;
+    }
+
+    public static String white(String input) {
+        return WHITE+input+RESET;
+    }
+
+    public static String black(String input) {
+        return BLACK+input+RESET;
+    }
+
 }
